6f8e7045de96048148186d130cff805442e6fac6
alyssa najera <anajera@bu.edu>
2025-04-29 14:54:05 -0400

    fixed suspense

---
+++ app/profile/Welcome.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import Image from "next/image";
+
+export default function Welcome() {
+    const searchParams = useSearchParams();
+    const name = searchParams.get('name');
+    const picture = searchParams.get('picture');
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-col items-center justify-center bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+                <h1 className="text-2xl font-bold mb-2 text-center text-black">Thank you {name} for trying the application!</h1>
+                <h2 className="text-lg mb-4  text-gray-600">CS391 is my favorite class!</h2>
+                <div className="flex justify-center mt-4">
+                    {picture && <Image
+                        src={picture}
+                        alt="profile picture"
+                        width={128}
+                        height={128}
+                        className="rounded-full"
+                    />}
+                </div>
+            </div>
+
+        </div>
+    );
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,31 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
 import { Suspense } from 'react';
-import Image from "next/image";
+import Welcome from './Welcome'; // or adjust import path
 
-export default function Welcome() {
-    const searchParams = useSearchParams();
-    const name = searchParams.get('name');
-    const picture = searchParams.get('picture');
-
+export default function ProfilePage() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-        <div className="flex justify-center items-center h-screen">
-            <div className="flex flex-col items-center justify-center bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-                <h1 className="text-2xl font-bold mb-2 text-center text-black">Thank you {name} for trying the application!</h1>
-                <h2 className="text-lg mb-4  text-gray-600">CS391 is my favorite class!</h2>
-                <div className="flex justify-center mt-4">
-                    {picture && <Image
-                        src={picture}
-                        alt="profile picture"
-                        width={128}
-                        height={128}
-                        className="rounded-full"
-                    />}
-                </div>
-            </div>
-
-        </div>
+            <Welcome />
         </Suspense>
     );
 }
